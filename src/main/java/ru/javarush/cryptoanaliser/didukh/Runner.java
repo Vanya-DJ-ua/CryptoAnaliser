99bbc652a79fd6c8a764686a02b4afc6e440372d
Vanya-DJ-ua <mister.didukh09021998@gmail.com>
2022-07-17 14:41:56 +0300

    init repo

--- src/main/java/ru/javarush/cryptoanaliser/didukh/Runner.java
+++ src/main/java/ru/javarush/cryptoanaliser/didukh/Runner.java
@@ -3,9 +3,14 @@ package ru.javarush.cryptoanaliser.didukh;
 import ru.javarush.cryptoanaliser.didukh.toplevel.Application;
 import ru.javarush.cryptoanaliser.didukh.controller.MainController;
 import ru.javarush.cryptoanaliser.didukh.entity.Result;
+import ru.javarush.cryptoanaliser.didukh.toplevel.Console;
+
+
 
 public class Runner {
     public static void main(String[] args) {
+        Console.start();
+        args = Console.commands;
         MainController mainController = new MainController();
         Application application = new Application(mainController);
         Result result = application.run(args);
